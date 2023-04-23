95ba18175243eef714ff6fe6202f5170fec1d2df
Collette Charles <collettecra123@gmail.com>
2023-04-23 15:27:08 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+document.addEventListener('DOMContentLoaded',fetchBooks);
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+  const main = document.querySelector('main') 
+  books.forEach(book => {   
+    const h2 = document.createElement('h2')    
+    h2.innerHTML = book.name    
+    main.appendChild(h2)  
+  })
+
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+
+function fetchBooks() {
+  // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(response => response.json())
+  .then(data => renderBooks(data));
+
+}
+fetchBooks()
+
+
+
+
+
+function initialize(){
+  fetchBooks()
+}
+initialize()
